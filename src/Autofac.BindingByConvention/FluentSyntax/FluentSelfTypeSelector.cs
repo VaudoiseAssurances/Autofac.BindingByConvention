864df29a261376fc0f13c308c9287cd76df43ffd
Fabio Salvalai <fabio@seesharp.ch>
2018-07-17 14:00:12 +0200

    [4-] remove redundant code

--- src/Autofac.BindingByConvention/FluentSyntax/FluentSelfTypeSelector.cs
+++ src/Autofac.BindingByConvention/FluentSyntax/FluentSelfTypeSelector.cs
@@ -1,50 +1,41 @@
 ﻿namespace Autofac.BindingByConvention.FluentSyntax
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
 
     using Autofac.Builder;
     using Autofac.Features.Scanning;
 
     public class FluentSelfTypeSelector
-
     {
         private readonly IRegistrationBuilder<object, ScanningActivatorData, DynamicRegistrationStyle> builder;
 
         public FluentSelfTypeSelector(
             IRegistrationBuilder<object, ScanningActivatorData, DynamicRegistrationStyle> builder)
-
         {
             this.builder = builder;
         }
 
         public FluentTypeFilter IsAssignableTo<T>()
-
         {
             return new FluentTypeFilter(this.builder, new Func<Type, bool>[] { type => type.IsAssignableTo<T>() });
         }
 
         public FluentTypeFilter MatchingPredicate(Func<Type, bool> predicate)
-
         {
             return new FluentTypeFilter(this.builder, new[] { predicate });
         }
 
         public FluentContractFilter NameMatching(Func<string, string, bool> predicate)
-
         {
-            this.builder.Where<object, ScanningActivatorData, DynamicRegistrationStyle>(
-                (Func<Type, bool>)(type => ((IEnumerable<Type>)type.GetInterfaces()).Any<Type>(
-                                          (Func<Type, bool>)(i => predicate(i.Name, type.Name)))));
+            this.builder.Where(
+                type => type.GetInterfaces().Any(
+                    i => predicate(i.Name, type.Name)));
 
             return new FluentContractFilter(
                 this.builder,
-                new Func<Type, Type, bool>[1]
-                    {
-                        (Func<Type, Type, bool>)((contract, implementation) =>
-                                                        predicate(contract.Name, implementation.Name))
-                    });
+                (Func<Type, Type, bool>)((contract, implementation) =>
+                                                predicate(contract.Name, implementation.Name)));
         }
     }
 }
